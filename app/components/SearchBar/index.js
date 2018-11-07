9f32041a48054c0299909e816957c4e7344d0164
Richard Cann <rmcp1g15@soton.ac.uk>
2018-11-07 18:31:26 +0000

    added search bar

---
+++ app/components/SearchBar/index.js
@@ -0,0 +1,19 @@
+// @flow
+import 'antd/lib/input/style/css';
+import React from 'react';
+import { Input } from 'antd';
+const Search = Input.Search;
+
+type BarProps = {
+  placholder: string
+}
+
+export default function SearchBar(props : BarProps){
+  const {placeholder} = props;
+  return(
+    <Search
+      placeholder={placeholder}
+      onSearch={value => console.log(value)}
+      enterButton
+    />)
+}
--- stories/index.js
+++ stories/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import CategoryButton from '../app/components/CategoryButton';
+import SearchBar from '../app/components/SearchBar';
 
 storiesOf('Button', module)
   .add('with text', () => (
-    <CategoryButton onClick={action('clicked')}>Hello Button</CategoryButton>
+    <CategoryButton onClick={action('clicked')} name="test buttton" />
+  ));
+
+storiesOf('SearchBar', module)
+  .add('with text', () => (
+    <SearchBar onClick={action('clicked')} placeholder="input text" />
   ));
