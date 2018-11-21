8af1c91d4f33a7ab8a2e6e99fb0a652b4e73b4af
Richard Cann <rmcp1g15@soton.ac.uk>
2018-11-21 08:47:45 +0000

    update twitter key

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.get('/twitter_trends', (req, res) => {
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
-      Authorization: `OAuth oauth_consumer_key="cDZT2V1kG05HWdcz3UbwlBfUk",oauth_token="604858048-61eSfrcP0KkfMPXo1sCPI1Nme3z1ZAOuMkZlL6Id",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1542671727",oauth_nonce="MGeyldGID0b",oauth_version="1.0",oauth_signature="LNWBLbm8tvOHThQY9bRRmG5hbQg%3D"`
+      Authorization: `OAuth oauth_consumer_key="cDZT2V1kG05HWdcz3UbwlBfUk",oauth_token="604858048-61eSfrcP0KkfMPXo1sCPI1Nme3z1ZAOuMkZlL6Id",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1542790000",oauth_nonce="7nR6vBwz4LB",oauth_version="1.0",oauth_signature="HKXzaBpQKHp4EPszXaFsw68pr78%3D"`
     },
   }, (err, r, body) => {
     if (err) {
